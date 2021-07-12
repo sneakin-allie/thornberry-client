444808bc42f535e1dcac477b76e7986ebc37f9f4
Allison Schulman <allison.d.schulman@gmail.com>
2021-07-12 08:07:12 -0400

    Added rel to href

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="Footer">
-                <p>© 2021 Allison Schulman. All rights reserved. Connect with me: <a href="https://www.linkedin.com/in/allisonschulman/" target="_blank">LinkedIn</a> <a href="https://github.com/sneakin-allie" target="_blank">GitHub</a></p>
+                <p>© 2021 Allison Schulman. All rights reserved. Connect with me: <a href="https://www.linkedin.com/in/allisonschulman/" target="_blank" rel="noreferrer">LinkedIn</a> <a href="https://github.com/sneakin-allie" target="_blank" rel="noreferrer">GitHub</a></p>
             </div>
         );
     }
