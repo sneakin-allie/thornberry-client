dddd1a78faa35d68272fd32dc53d262a7fc0c8c0
Allison Schulman <allison.d.schulman@gmail.com>
2021-07-12 07:56:20 -0400

    Added links to Footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="Footer">
-                <p>© 2021 Allison Schulman. All rights reserved.</p>
+                <p>© 2021 Allison Schulman. All rights reserved. Connect with me: <a href="https://www.linkedin.com/in/allisonschulman/" target="_blank">LinkedIn</a> <a href="https://github.com/sneakin-allie" target="_blank">GitHub</a></p>
             </div>
         );
     }
